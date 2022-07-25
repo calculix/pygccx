cc273e0116e7d5e97ada463652d69db746b4324f
Black-Winterheart <Winterheart@gmx.de>
2022-07-25 21:54:28 +0200

    Added unit test for *CONTACT FILE

--- pygccx/test_step_features.py
+++ pygccx/test_step_features.py
@@ -3,6 +3,7 @@ from step_features.test.test_step import TestStep
 from step_features.test.test_static import TestStatic
 from step_features.test.test_cload import TestCload
 from step_features.test.test_time_points import TestTimePoints
+from step_features.test.test_contact_file import TestContactFile
 
 
 if __name__ == '__main__':
